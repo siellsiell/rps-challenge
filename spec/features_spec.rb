3bee49a422b3b200a476b03062c3004f3f222be8
Simo Tchokni <{ID}+{username}@users.noreply.github.com>
2021-01-29 18:54:47 +0000

    Fix regression that caused draws to stop working.

--- spec/features_spec.rb
+++ spec/features_spec.rb
@@ -7,7 +7,7 @@ require "game"
 feature 'Testing setup' do
   scenario "Can run app and check page content" do
     visit '/'
-    expect(page).to have_content("What's your name?")
+    expect(page).to have_content(/Play RPS?/)
   end
 end
 
@@ -17,26 +17,12 @@ feature 'Can submit player name and see name on screen' do
       visit '/'
       fill_in('name', with: name)
       click_on('Play')
-      #expect(page).to have_current_path("/play?name=")
       expect(page).to have_content("Hi #{name}, let's play!")
     end
   end
 end
 
 
-# feature 'Can choose rock, paper or scissors' do
-#   ["rock", "paper", "scissors"].each do |option|
-#     scenario "Click on #{option}, opponent choice and winner is announced" do
-#       player = "simo"
-#       visit '/'
-#       fill_in('name', with: player)
-#       click_on('Play')
-#       click_on(option)
-#       expect(page).to have_content(/#{player} chose #{option}. Bot chose (rock|paper|scissors)./)
-#       expect(page).to have_content(/(It's a draw|(#{option}|Bot) wins)!/)
-#     end
-#   end
-# end
 
 feature 'Can choose rock, paper or scissors and find out game outcome' do
 
